6e34b30af2cf72ad3223f874dac453e3defdb951
Vince Neuda <neudavince@gmail.com>
2025-10-02 19:15:46 +0100

    add basic new (create) route and template

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const campModel = require('./models/campground');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/YelpCampDB')   //connects to local DB (or creates if it does not already exists)
+mongoose.connect('mongodb://127.0.0.1:27017/YelpCampDB')    //connects to local DB (or creates if it does not already exists)
     .then(() => console.log('MongoDB connected'))           //successful connection
     .catch(err => console.error('Connection error:', err)); //failed connection
 
@@ -16,6 +16,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/YelpCampDB')   //connects to local D
 app.engine('ejs', ejsMate)
 app.set('views', path.join(__dirname, 'views')); //tells Express where to find 'views' folder containing templates
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true })); //parse incoming data sent by <form> submissions
+
+
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -25,6 +29,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await campModel.find({});
     res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new campModel(req.body.campground)
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
